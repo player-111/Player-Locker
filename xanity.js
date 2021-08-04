ce34f1fe33dd712164ffbb5abd146ba5e9d46c06
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 12:21:54 +0530

    Create xanity.js

---
+++ xanity.js
@@ -0,0 +1,28 @@
+var mineflayer = require('mineflayer');
+
+var options = {
+    host: 'mc.mineberry.net',
+    username: 'Xaniity',
+    version: "1.8.9",
+};
+
+var bot = mineflayer.createBot(options);
+bindEvents(bot);
+
+function bindEvents(bot) {
+
+    bot.on('error', function(err) {
+        console.log("Bot has encountered an error");
+    });
+
+    bot.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options);
+        bindEvents(bot);
+    }
+}
