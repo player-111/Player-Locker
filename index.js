64b41b26feca46c29c62db52f3b8d2d397151689
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 12:08:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,57 +2,19 @@ var mineflayer = require('mineflayer');
 
 var options = {
     host: 'mc.mineberry.net',
-    username: 'iceplant',
-    version: "1.8.9",
-}
-var options1 = {
-    host: 'mc.mineberry.net',
     username: 'andre2dolars',
     version: "1.8.9",
-}
-var options2 = {
-    host: 'mc.mineberry.net',
-    username: 'Xaniity',
-    version: "1.8.9",
-}
-var options3 = {
-    host: 'mc.mineberry.net',
-    username: 'GhostGirl_',
-    version: "1.8.9",
-}
-var options4 = {
-    host: 'mc.mineberry.net',
-    username: 'Abyys_',
-    version: "1.8.9",
-}
-var options5 = {
-    host: 'mc.mineberry.net',
-    username: 'GhostGirl_',
-    version: "1.8.9",
-}
-var options6 = {
-    host: 'mc.mineberry.net',
-    username: 'Jfreefortnite',
-    version: "1.8.9",
-}
-var options7 = {
-    host: 'mc.mineberry.net',
-    username: 'Zakreedanub',
-    version: "1.8.9",
-}
-var options8 = {
-    host: 'mc.mineberry.net',
-    username: 'Hauntear',
-    version: "1.8.9",
-}
-var options9 = {
-    host: 'mc.mineberry.net',
-    username: 'StingEucliffe_',
-    version: "1.8.9",
-}
+};
+
 var bot = mineflayer.createBot(options);
 bindEvents(bot);
+
 function bindEvents(bot) {
+
+    bot.on('error', function(err) {
+        console.log("Bot has encountered an error");
+    });
+
     bot.on('end', function() {
         console.log("Bot has ended");
         setTimeout(relog, 0);
@@ -61,20 +23,6 @@ function bindEvents(bot) {
     function relog() {
         console.log("reconnecting");
         bot = mineflayer.createBot(options);
-        bindEvents(bot);
-    }
-}
-var client = mineflayer.createBot(options1);
-bindEvents(client);
-function bindEvents(client) {
-    client.on('end', function() {
-        console.log("Bot has ended");
-        setTimeout(relog, 0);
-    });
-
-    function relog() {
-        console.log("reconnecting");
-        bot = mineflayer.createBot(options1);
         bindEvents(bot);
     }
 }
