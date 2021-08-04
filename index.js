6b7c0b52724a6e1481e4456a8996ea537078f1b0
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 11:42:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,28 @@ var mineflayer = require('mineflayer');
 
 var options = {
     host: 'mc.mineberry.net',
-    username: 'iceplant',
+    port: 25565,
+    username: 'name',
     version: "1.8.9",
-}
-var options1 = {
-    host: 'mc.mineberry.net',
-    username: 'andre2dolars',
-    version: "1.8.9",
-}
-var options2 = {
-    host: 'mc.mineberry.net',
-    username: 'Xaniity',
-    version: "1.8.9",
-}
-var options3 = {
-    host: 'mc.mineberry.net',
-    username: 'GhostGirl_',
-    version: "1.8.9",
-}
-var options4 = {
-    host: 'mc.mineberry.net',
-    username: 'Abyys_',
-    version: "1.8.9",
-}
-var options5 = {
-    host: 'mc.mineberry.net',
-    username: 'GhostGirl_',
-    version: "1.8.9",
-}
+};
 
 var bot = mineflayer.createBot(options);
 bindEvents(bot);
-var bot1 = mineflayer.createBot(options1);
-bindEvents(bot1);
-var bot2 = mineflayer.createBot(options2);
-bindEvents(bot2);
-var bot3 = mineflayer.createBot(options3);
-bindEvents(bot3);
-var bot4 = mineflayer.createBot(options4);
-bindEvents(bot4);
-var bot5 = mineflayer.createBot(options5);
-bindEvents(bot5);
+
+function bindEvents(bot) {
+
+    bot.on('error', function(err) {
+        console.log("Bot has encountered an error");
+    });
+
+    bot.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options);
+        bindEvents(bot);
+    }
+}
