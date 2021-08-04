6955fb145cf86d8fc22925473387eb9c3b1c4f81
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 11:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,4 +45,73 @@ function bindEvents(bot) {
         bindEvents(bot);
     }
 }
+var bot1 = mineflayer.createBot(options1);
+bindEvents(bot1);
+function bindEvents(bot1) {
+    bot1.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
 
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options1);
+        bindEvents(bot);
+    }
+}
+var bot2 = mineflayer.createBot(options2);
+bindEvents(bot2);
+function bindEvents(bot2) {
+    bot2.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options2);
+        bindEvents(bot);
+    }
+}
+var bot3 = mineflayer.createBot(options3);
+bindEvents(bot3);
+function bindEvents(bot3) {
+    bot3.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options3);
+        bindEvents(bot);
+    }
+}
+var bot4 = mineflayer.createBot(options4);
+bindEvents(bot4);
+function bindEvents(bot4) {
+    bot4.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options4);
+        bindEvents(bot);
+    }
+}
+var bot5 = mineflayer.createBot(options5);
+bindEvents(bot5);
+function bindEvents(bot5) {
+    bot5.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options5);
+        bindEvents(bot);
+    }
+}
