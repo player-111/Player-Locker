a2b7857ef0b75a0ebbf7e20f555c732bab81dce9
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 11:55:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,26 @@ var options5 = {
     username: 'GhostGirl_',
     version: "1.8.9",
 }
-
+var options6 = {
+    host: 'mc.mineberry.net',
+    username: 'Jfreefortnite',
+    version: "1.8.9",
+}
+var options7 = {
+    host: 'mc.mineberry.net',
+    username: 'Zakreedanub',
+    version: "1.8.9",
+}
+var options8 = {
+    host: 'mc.mineberry.net',
+    username: 'Hauntear',
+    version: "1.8.9",
+}
+var options9 = {
+    host: 'mc.mineberry.net',
+    username: 'StingEucliffe_',
+    version: "1.8.9",
+}
 var bot = mineflayer.createBot(options);
 bindEvents(bot);
 function bindEvents(bot) {
@@ -45,10 +64,10 @@ function bindEvents(bot) {
         bindEvents(bot);
     }
 }
-var bot1 = mineflayer.createBot(options1);
-bindEvents(bot1);
-function bindEvents(bot1) {
-    bot1.on('end', function() {
+var client = mineflayer.createBot(options1);
+bindEvents(client);
+function bindEvents(bot) {
+    bot.on('end', function() {
         console.log("Bot has ended");
         setTimeout(relog, 0);
     });
@@ -56,62 +75,6 @@ function bindEvents(bot1) {
     function relog() {
         console.log("reconnecting");
         bot = mineflayer.createBot(options1);
-        bindEvents(bot);
-    }
-}
-var bot2 = mineflayer.createBot(options2);
-bindEvents(bot2);
-function bindEvents(bot2) {
-    bot2.on('end', function() {
-        console.log("Bot has ended");
-        setTimeout(relog, 0);
-    });
-
-    function relog() {
-        console.log("reconnecting");
-        bot = mineflayer.createBot(options2);
-        bindEvents(bot);
-    }
-}
-var bot3 = mineflayer.createBot(options3);
-bindEvents(bot3);
-function bindEvents(bot3) {
-    bot3.on('end', function() {
-        console.log("Bot has ended");
-        setTimeout(relog, 0);
-    });
-
-    function relog() {
-        console.log("reconnecting");
-        bot = mineflayer.createBot(options3);
-        bindEvents(bot);
-    }
-}
-var bot4 = mineflayer.createBot(options4);
-bindEvents(bot4);
-function bindEvents(bot4) {
-    bot4.on('end', function() {
-        console.log("Bot has ended");
-        setTimeout(relog, 0);
-    });
-
-    function relog() {
-        console.log("reconnecting");
-        bot = mineflayer.createBot(options4);
-        bindEvents(bot);
-    }
-}
-var bot5 = mineflayer.createBot(options5);
-bindEvents(bot5);
-function bindEvents(bot5) {
-    bot5.on('end', function() {
-        console.log("Bot has ended");
-        setTimeout(relog, 0);
-    });
-
-    function relog() {
-        console.log("reconnecting");
-        bot = mineflayer.createBot(options5);
         bindEvents(bot);
     }
 }
