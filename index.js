6fbd79f1a6cdad9d74242b2046ab1a9d41698cd4
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 12:30:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const xanity = require('./xanity.js');
 const jfree = require('./jfree.js');
 const abyys = require('./abyys.js');
 const hauntear = require('./haunter.js');
+const daddy = require('./daddy.js');
 var options = {
     host: 'mc.mineberry.net',
     username: 'andre2dolars',
