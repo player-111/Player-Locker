6fae5107384c4a2273ed6a05be15fd466280b41e
Shadow <59509353+Phoenix-Development@users.noreply.github.com>
2021-08-04 16:03:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ var options2 = {
     username: 'Hauntear',
     version: "1.8.9",
 };
+var options3 = {
+    host: 'mc.mineberry.net',
+    username: 'iceplant',
+    version: "1.8.9",
+};
+var options4 = {
+    host: 'mc.mineberry.net',
+    username: 'Xaniity',
+    version: "1.8.9",
+};
 //lucy
 var lucy = mineflayer.createBot(options1);
 bindEvents(lucy);
@@ -31,8 +41,8 @@ function bindEvents(lucy) {
 
     function relog() {
         console.log("reconnecting");
-        bot = mineflayer.createBot(options);
-        bindEvents(lucy );
+        lucy = mineflayer.createBot(options);
+        bindEvents(lucy);
     }
 }
 //andre
@@ -57,3 +67,44 @@ function bindEvents(bot) {
     }
 }
 //hauntear
+var hauntear = mineflayer.createBot(options2);
+bindEvents(hauntear);
+
+function bindEvents(hauntear) {
+
+    hauntear.on('error', function(err) {
+        console.log("Bot has encountered an error");
+    });
+
+    hauntear.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        hauntear = mineflayer.createBot(options2);
+        bindEvents(hauntear);
+    }
+}
+//ice
+var ice = mineflayer.createBot(options3);
+bindEvents(ice);
+
+function bindEvents(ice) {
+
+    ice.on('error', function(err) {
+        console.log("Bot has encountered an error");
+    });
+
+    ice.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        ice = mineflayer.createBot(options3);
+        bindEvents(ice);
+    }
+}
