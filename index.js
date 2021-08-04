dfec41c5c3fd318ad1db1198a51f45e5b6796ac5
hecker-men <88416468+hecker-men@users.noreply.github.com>
2021-08-04 11:11:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,3 +26,31 @@ function bindEvents(bot) {
         bindEvents(bot);
     }
 }
+
+var options1 = {
+    host: 'mc.mineberry.net',
+    username: 'andre2dolars',
+    version: "1.8.9",
+};
+
+var bot1 = mineflayer.createBot(options1);
+bindEvents(bot1);
+
+function bindEvents(bot1) {
+
+    bot1.on('error', function(err) {
+        console.log("Bot has encountered an error");
+    });
+
+    bot1.on('end', function() {
+        console.log("Bot has ended");
+        setTimeout(relog, 0);
+    });
+
+    function relog() {
+        console.log("reconnecting");
+        bot = mineflayer.createBot(options1);
+        bindEvents(bot);
+    }
+}
+
