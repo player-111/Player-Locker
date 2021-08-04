697b8d55cdd3cdf2ae7a254886de8ee0941152d8
Shadow <59509353+Phoenix-Development@users.noreply.github.com>
2021-08-04 13:12:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var mineflayer = require('mineflayer');
-const ice = require('./players/iceplant.js');
 const sting = require('./players/sting.js');
 const lucy = require('./players/lucy.js');
-const zak = require('./players/zak.js');
-const tim = require('./players/tim.js');
-const dxtr = require('./players/dxtr.js');
 const xanity = require('./players/xanity.js');
-const jfree = require('./players/jfree.js');
 const abyys = require('./players/abyys.js');
 const daddy = require('./players/daddy.js');
-const egg = require('./players/eggplant.js');
-const hauntear = require('./players/hauntear.js');
 const andre = require('./players/andre')
 
